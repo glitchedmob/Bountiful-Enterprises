4f67f90422c642ab1fd39fb0f9b306f10c8051c9
Levi Zitting <levizitting5663@gmail.com>
2015-05-02 23:21:22 -0500

    Service
    Reworked the services page and added in the modals

--- js/pages/projects.js
+++ js/pages/projects.js
@@ -1,16 +1,16 @@
 
 $(document).ready(function() {
-    $('#year-list .project-year').click(function() {
+	$('#year-list .project-year').click(function() {
 
-        if (this.flipped) {
-        	$(this).next('.projects').slideToggle(400);
-            $(this).find("i", this).removeClass('fa-rotate-180');
-            this.flipped = false;
-        } else {
-            $(this).next('.projects').slideToggle(400);
-            $(this).find("i").addClass('fa-rotate-180');
-            this.flipped = true;
-        }
+		if (this.flipped) {
+			$(this).next('.projects').slideToggle(400);
+			$(this).find("i", this).removeClass('fa-rotate-180');
+			this.flipped = false;
+		} else {
+			$(this).next('.projects').slideToggle(400);
+			$(this).find("i").addClass('fa-rotate-180');
+			this.flipped = true;
+		}
 
-    });
+	});
 });
