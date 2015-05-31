ae04ac0ee15d21b2871d138b664da8735ee7be91
Levi Zitting <levizitting5663@gmail.com>
2015-05-31 09:16:10 -0500

    Parallax
    Added  a parallax effect to the services page and replaced the tekla
    image with a higher reslution

--- js/pages/services.js
+++ js/pages/services.js
@@ -1,15 +1,21 @@
-$(function() {
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top
-				}, 1000);
-				return false;
-			}
-		}
-	});
-});
+$(document).ready(function(){
+   // cache the window object
+   $window = $(window);
+ 
+   $('div[data-type="background"]').each(function(){
+	 // declare the variable to affect the defined data-type
+	 var $scroll = $(this);
+					 
+	  $(window).scroll(function() {
+		// HTML5 proves useful for helping with creating JS functions!
+		// also, negative value because we're scrolling upwards                             
+		var yPos = -($window.scrollTop() / $scroll.data('speed')); 
+		 
+		// background position
+		var coords = '50% '+ yPos + 'px';
+ 
+		// move the background
+		$scroll.css({ backgroundPosition: coords });    
+	  }); // end window scroll
+   });  // end section function
+}); // close out script
